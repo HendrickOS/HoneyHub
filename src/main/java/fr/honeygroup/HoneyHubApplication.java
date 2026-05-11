cefc7f80984575774508b457c472886ba99eb89d
HendrickOS <osseux.hendrick@gmail.com>
2026-05-11 11:29:47 +0200

    Initial Commit

---
+++ src/main/java/fr/honeygroup/HoneyHubApplication.java
@@ -0,0 +1,13 @@
+package fr.honeygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/honeygroup/HoneyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.honeygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
