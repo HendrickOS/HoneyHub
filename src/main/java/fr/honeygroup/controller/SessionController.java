2aa3ce0ab2533aa150c64d45cbeb73220851e8de
fizazi200 <fizazi2021@gmail.com>
2026-05-28 14:10:45 +0200

    add croosorigin tosessioncontrolleur

--- src/main/java/fr/honeygroup/controller/SessionController.java
+++ src/main/java/fr/honeygroup/controller/SessionController.java
@@ -3,6 +3,7 @@ package fr.honeygroup.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/sessions")
 @RequiredArgsConstructor
+@CrossOrigin
 public class SessionController {
 
     private final SessionService sessionService;
